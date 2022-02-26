e23bdb875ebb577871805fcd45807cf48386b57a
User <addisumm@gmail.com>
2022-02-26 05:36:50 -0600

    Exception handling in springBoot

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,9 +1,11 @@
 package com.example.demo.customer;
 
+import com.example.demo.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -20,5 +22,14 @@ public class CustomerService {
     List <Customer> getcustomers () {
 
         return customerRepo.getCustomers ();
+    }
+
+    public Customer getCustomers (@PathVariable("customerId") Long id){
+        return
+                getcustomers ()
+                        .stream ()
+                        .filter (cust->cust.getId ().equals (id))
+                        .findFirst ()
+                        .orElseThrow (()-> new NotFoundException ("Sorry, but customer with Id: "+id+" not found"));
     }
 }
